af0e5027ba7ef35f37e1b0768e514ed03c5e46ce
mustafacoskunn <l1621012067@stud.sdu.edu.tr>
2019-10-29 18:29:59 +0300

    tasarım

--- app/src/main/java/com/tanitim/universitetanitim/DetayActivity.java
+++ app/src/main/java/com/tanitim/universitetanitim/DetayActivity.java
@@ -1,27 +1,59 @@
 package com.tanitim.universitetanitim;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jaeger.library.StatusBarUtil;
+
+
+import com.squareup.picasso.Picasso;
 import com.tanitim.universitetanitim.Models.Universiteler;
 
-public class DetayActivity extends AppCompatActivity {
+public class DetayActivity extends BaseActivity {
     private TextView textViewIsim;
     private TextView textViewID;
     private Universiteler universite;
+    private Toolbar mToolbar;
+    private View mViewNeedOffset;
+    private ImageView detayImage;
+
+
+
+    private int mAlpha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detay);
 
+      //  textViewIsim = findViewById(R.id.textViewIsim);
+      //  textViewID = findViewById(R.id.textViewID);
+
+
+        detayImage = findViewById(R.id.detayImage);
+        mToolbar = findViewById(R.id.toolbar);
+        mViewNeedOffset = findViewById(R.id.view_need_offset);
+        StatusBarUtil.setTranslucentForImageView(DetayActivity.this, 100, mViewNeedOffset);
+
+
+        setSupportActionBar(mToolbar);
         textViewIsim = findViewById(R.id.textViewIsim);
         textViewID = findViewById(R.id.textViewID);
 
         universite = (Universiteler) getIntent().getSerializableExtra("nesne");
         textViewIsim.setText(universite.getIsim());
         textViewID.setText(universite.getBolge()+" BÖLGESİ");
+
+
+        Picasso.with(DetayActivity.this)
+                .load("https://takipgym.com/resim/"+universite.getSlug()+".webp")
+                .into(detayImage);
+
 
         Log.e("getAdres:",universite.getAdres());
         Log.e("getEposta:",universite.getEposta());
@@ -41,4 +73,12 @@ public class DetayActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    protected void setStatusBar() {
+        mViewNeedOffset = findViewById(R.id.view_need_offset);
+        StatusBarUtil.setTranslucentForImageView(this, mViewNeedOffset);
+    }
+
+
 }
